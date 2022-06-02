fcf82501dc6da910c3b00f33271b1ff3b3282b6d
shohei118 <shohei.201156@gmail.com>
2022-06-02 15:07:04 +0900

    propatiesにpostgresを追加

--- TodoManager/src/main/java/com/example/demo/domain/todo.java
+++ TodoManager/src/main/java/com/example/demo/domain/todo.java
@@ -1,5 +1,85 @@
 package com.example.demo.domain;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "todo")
 public class todo {
+	
+	@Id
+	private int todo_id;
+
+	private String todo;
+	
+	private LocalDate date;
+	
+	private String title;
+	
+	private int importance;
+	
+	private int urgency;
+	
+	private String user_id;
+	
+	
+	public int getTodo_id() {
+		return todo_id;
+	}
+
+	public void setTodo_id(int todo_id) {
+		this.todo_id = todo_id;
+	}
+
+	public String getTodo() {
+		return todo;
+	}
+
+	public void setTodo(String todo) {
+		this.todo = todo;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getImportance() {
+		return importance;
+	}
+
+	public void setImportance(int importance) {
+		this.importance = importance;
+	}
+
+	public int getUrgency() {
+		return urgency;
+	}
+
+	public void setUrgency(int urgency) {
+		this.urgency = urgency;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
 
 }
--- TodoManager/src/main/java/com/example/demo/domain/todo_user.java
+++ TodoManager/src/main/java/com/example/demo/domain/todo_user.java
@@ -1,4 +1,4 @@
-package com.example.demo.domain.entity;
+package com.example.demo.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
